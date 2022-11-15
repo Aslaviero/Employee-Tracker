dd429030a9df1b6397c7901a74b220bdd1dede8b
Anthony Slaviero <tonyslaviero@gmail.com>
2022-11-14 17:14:34 -0700

    got added department working

--- server.js
+++ server.js
@@ -2,8 +2,7 @@
 const express = require('express');
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
-const cTable = require('./node_modules/console.table');
-
+const cTable = require('console.table');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -16,30 +15,39 @@ app.use((req, res) => {
  res.status(404).end();
 });
 
-db.connect(function(err) {
-  if (err) {
-    return console.error('error: ' + err.message);
-  }
-  console.log('Connected to the employeeTracker database.');
+// Connect to database
+const db = mysql.createConnection(
+  {
+      host: '127.0.01',
+      user: 'root',
+      password: 'root',
+      database: 'employeetracker'
+  },
+);
+
+db.connect(err => {
+  if (err) throw err;
+  console.log("\n WELCOME TO EMPLOYEE TRACKER \n");
+    startPrompt();
 });
+
   
 
 //Main menu that you first see when doing node server.js
-function mainMenu() {
-  inquirer.prompt([
+function startPrompt() {
+  inquirer.prompt(
     {
       type: 'list',
-      name: 'Mainmenu',
+      name: 'mainMenu',
       message: 'What would you like to do?',
       choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Delete Employee', 'Delete Role', 'Delete Department', 'Quit']
-    }
-  ]).then((answers) => {
-    switch (answers.Mainmenu) {
+    }).then((answers) => {
+    switch (answers.mainMenu) {
       case 'View All Employees':
         viewAllEmployees();
         break;
       case 'Add Employee':
-        AddEmployee();
+        addEmployee();
         break;
       case 'Update Employee Role':
         updateEmployeeRole();
@@ -48,13 +56,13 @@ function mainMenu() {
         viewAllRoles();
         break;
       case 'Add Role':
-        AddRole();
+        addRole();
         break;
       case 'View All Departments':
         viewAllDepartments();
         break;
       case 'Add Department':
-        addDempartment();
+        addDepartment();
         break;
       case 'Delete Employee':
         deleteEmployee();
@@ -73,18 +81,44 @@ function mainMenu() {
 };
 
 // View All Employees
-function viewAllEmployees() {
+ viewAllEmployees = () => {
   db.connect(function (err) {
     if (err) throw err;
     db.query("SELECT * FROM employee", function (err, result) {
       if (err) throw err;
       console.table(result);
+      startPrompt();
     });
   });
 };
 
+// view All Roles
+function viewAllRoles() {
+  db.connect(function (err) {
+    if (err) throw err;
+    db.query("SELECT * FROM roles", function (err, result) {
+      if (err) throw err;
+      console.table(result);
+      startPrompt();
+    });
+  });
+};
+
+// view All Departments
+function viewAllDepartments() {
+  db.connect(function (err) {
+    if (err) throw err;
+    db.query("SELECT * FROM department", function (err, result) {
+      if (err) throw err;
+      console.table(result);
+      startPrompt();
+    });
+  });
+};
+  
+
 // Add Employee
-function AddEmployee() {
+function addEmployee() {
   inquirer.prompt([
     {
       type: 'input',
@@ -107,30 +141,14 @@ function AddEmployee() {
       message: 'Enter ID of new employees manager'
     }
   ]).then((result, err) => {
-    db.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)', [res.first_name, res.last_name, res.role_id, res.manager_id] + `SELECT * FROM employee`)
+    db.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)', [result.first_name, result.last_name, result.role_id, result.manager_id] + `SELECT * FROM employee`)
     if (err) throw err;
     console.table(result);
   })
 };
 
-// Update Employee Role
-function updateEmployeeRole() {
-
-}
-
-// view All Roles
-function viewAllRoles() {
-  db.connect(function (err) {
-    if (err) throw err;
-    db.query("SELECT * FROM roles", function (err, result) {
-      if (err) throw err;
-      console.table(result);
-    });
-  });
-};
-
 // Add Role
-function AddRole() {
+function addRole() {
   inquirer.prompt([
     {
       type: 'input',
@@ -148,45 +166,45 @@ function AddRole() {
       message: 'Enter department id for new role'
     }
   ]).then((result, err) => {
-    db.query('INSERT INTO roles (title, salary, deparment_id) VALUES (?, ?, ?)', [result.title, result.salary, result.department_id])
+    db.query('INSERT INTO roles (title, salary, deparment_id) VALUES (?, ?, ?)', [result.title, result.salary, result.department_id] + `SELECT * FROM roles`)
     if (err) throw err;
     console.table(result);
   });
 };
 
+// Add Department
+function addDepartment() {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'department_name',
+      message: 'Create new Department'
+    }
+  ]).then((answer) => {
+    db.query(`INSERT INTO department (department_name) VALUES ("${answer.department_name}");`,
+    (err) => {
+      if(err) return err;
+      console.log('New Department Added!');
+      startPrompt();
+    
+  });
+});
+};
+
+// Update Employee Role
+function updateEmployeeRole() {
+
+}
+
 // view All Departments
-function viewAllDepartments() {
+function viewEmployeesByDepartment() {
   db.connect(function (err) {
     if (err) throw err;
     db.query("SELECT * FROM department", function (err, result) {
       if (err) throw err;
       console.table(result);
+      startPrompt();
     });
-  });
-};
-
-// add Dempartment
-function addDempartment() {
-  inquirer.prompt([
-    {
-      type: 'input',
-      name: 'title',
-      message: 'Create new Department'
-    },
-    {
-      type: 'number',
-      name: 'salary',
-      message: 'Enter the salary amount for the new department'
-    },
-    {
-      type: 'number',
-      name: 'department_id',
-      message: 'Enter department id for new department'
-    }
-  ]).then((result, err) => {
-    db.query('INSERT INTO departments (title, salary, deparment_id) VALUES (?, ?, ?)',[result.title, result.salary, result.department_id])
-    if(err) throw err;
-    console.table(result);
   });
 };
 
